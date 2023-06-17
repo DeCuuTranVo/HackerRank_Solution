2d40ac4195713a673c2d22812717112501d60f6c
DeCuuTranVo <quangtran.vo.9@gmail.com>
2023-06-17 19:05:22 +0700

    two more problems in linked list:

---
+++ Medium/Data Structures/Linked Lists/Delete duplicate-value nodes from a sorted linked list/delete_duplicate_value_nodes_from_a_sorted_linked_list.py
@@ -0,0 +1,92 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+#
+# Complete the 'removeDuplicates' function below.
+#
+# The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+# The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+def removeDuplicates(llist):
+    # Write your code here
+    head = llist
+    curr = head
+    
+    # when the next node is not None
+    while (curr.next is not None):
+        # if current node's value equals next node's value
+        if curr.data == curr.next.data:
+            # remove the next node
+            curr.next = curr.next.next
+        else: # if the next node's value is different from current node's value
+            # move the pointer from current node to next node
+            curr = curr.next
+            
+    return head
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = removeDuplicates(llist.head)
+
+        print_singly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
